29b1eeb1ad4fd60183d9cf104f2cd9b985819ad2
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-01-29 22:27:32 +0000

    test: updating unit test for button
    updating unit test for button
    
    PEL-85

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,11 +4,31 @@ import { render, screen } from "@testing-library/react";
 
 import Button from "@/components/Button/Button";
 
-describe("Adding test as initial starting point", () => {
-  it("renders a heading", () => {
+describe("Testing button component", () => {
+  it("renders a button with variant text", () => {
     const buttonText = "Text Button";
 
     render(<Button variant="text">{buttonText}</Button>);
+
+    const button = screen.getByText(buttonText);
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders a button with variant outlined", () => {
+    const buttonText = "Text Button";
+
+    render(<Button variant="outlined">{buttonText}</Button>);
+
+    const button = screen.getByText(buttonText);
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders a button with variant contained", () => {
+    const buttonText = "Text Button";
+
+    render(<Button variant="contained">{buttonText}</Button>);
 
     const button = screen.getByText(buttonText);
 
