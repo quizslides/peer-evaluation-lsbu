d537c6c3ebcf4041e761fdccd4aff1e0313ebd2e
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-02-21 22:38:04 +0000

    feat: adding notifications handler
    adding notifications using react-hot-toast
    
    PEL-85

---
+++ src/utils/notifications/index.ts
@@ -0,0 +1,48 @@
+import { toast } from "react-hot-toast";
+
+type TPromiseNotifications = {
+  loading: string;
+  success: string;
+  error: string;
+};
+
+const successNotification = (message: string, id?: string) => {
+  return toast.success(message, {
+    id,
+  });
+};
+
+const blankNotification = (message: string, id?: string): string => {
+  return toast(message, {
+    id,
+  });
+};
+
+const errorNotification = (message: string, id?: string): string => {
+  return toast.error(message, {
+    id,
+  });
+};
+
+const loadingNotification = (message: string, id?: string): string => {
+  return toast.loading(message, {
+    id,
+  });
+};
+
+const promiseNotification = (promise: Promise<any>, config: TPromiseNotifications): Promise<any> => {
+  return toast.promise(promise, config);
+};
+
+const dismissNotification = (id?: string) => {
+  toast.dismiss(id);
+};
+
+export {
+  blankNotification,
+  dismissNotification,
+  errorNotification,
+  loadingNotification,
+  promiseNotification,
+  successNotification,
+};
