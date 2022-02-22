e0860b23379b49df46282f3362449ded569b52b6
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-02-22 19:17:33 +0000

    feat: pages for sign-in and sign-out
    adding pages for sign-in and sign-out
    
    PEl-85

---
+++ src/pages/auth/signout.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+
+import type { NextPage } from "next";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { LoadingContainer } from "@/containers";
+import { loadingNotification, successNotification } from "@/utils";
+
+const SignOut: NextPage = () => {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const notificationsId = loadingNotification("Closing your session...", "signOut");
+
+    signOut({ redirect: false, callbackUrl: "/" }).then((data) => {
+      successNotification("Bye Bye", notificationsId);
+
+      router.push(data.url);
+    });
+  }, [router, session]);
+
+  if (session) {
+    return <LoadingContainer loading={!!session} />;
+  }
+
+  return <></>;
+};
+
+export default SignOut;
