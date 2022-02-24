780ef6508c2b0acdd9eb5e0f373316d03472a5ea
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-02-24 00:51:41 +0000

    feat: adding new select component
    adding select component
    
    PEL-85

---
+++ src/components/SelectFieldForm/SelectFieldForm.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Form, Formik } from "formik";
+import { object } from "yup";
+
+import { SelectFieldForm } from "@/components";
+import { RoleSelect, roleValidator } from "@/utils";
+
+describe("Testing SelectFieldForm component in a form", () => {
+  it("renders a SelectFieldForm with test id", async () => {
+    const initialFormState = {
+      role: "",
+    };
+
+    const validationSchema = object({
+      ...roleValidator,
+    });
+
+    const fieldName = "role";
+
+    const fieldLabel = "Role";
+
+    const testId = "test-id";
+
+    render(
+      <Formik initialValues={initialFormState} validationSchema={validationSchema} onSubmit={() => console.log(null)}>
+        {() => (
+          <Form>
+            <SelectFieldForm
+              testId={testId}
+              name={fieldName}
+              options={RoleSelect}
+              props={{
+                fullWidth: true,
+                label: fieldLabel,
+              }}
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    );
+
+    const element = screen.getByTestId(testId);
+
+    expect(element).toBeInTheDocument();
+  });
+});
