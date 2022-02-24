86248ea0db84777e51adeb4f6cdc32a875322c94
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-02-24 00:56:48 +0000

    test: fixing test with unwanted import

--- src/components/SelectFieldForm/SelectFieldForm.test.tsx
+++ src/components/SelectFieldForm/SelectFieldForm.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { Form, Formik } from "formik";
 import { object } from "yup";
 
