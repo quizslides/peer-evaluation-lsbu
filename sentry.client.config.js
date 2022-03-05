48d3731058bae1e7df4e8b0d1f0ac5801ce2affb
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-03-05 18:53:52 +0000

    feat: updating sentry to new project using google workspace
    updating sentry to new project using google workspace
    
    PEL-121

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,6 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.ENVIRONMENT,
+  environment: process.env.LOGICAL_ENVIRONMENT,
   tracesSampleRate: 1.0,
 });
