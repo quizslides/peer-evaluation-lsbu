b0e7be794c18aae02538c8b9e5c2c495d8699fc2
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-03-06 07:53:20 +0000

    fix: updating sentry environment for the banckend

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,6 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  environment: process.env.ENVIRONMENT,
+  environment: process.env.LOGICAL_ENVIRONMENT,
   tracesSampleRate: 1.0,
 });
