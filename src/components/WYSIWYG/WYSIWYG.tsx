0e6087c6d2e0e3a5b36a97e49fa7adc49acab113
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-03-14 10:21:04 +0000

    fix: adding a fix for SSR for the WYSIWYG
    adding a fix for SSR for the WYSIWYG not rendering correctly on the client
    
    PEL-92

--- src/components/WYSIWYG/WYSIWYG.tsx
+++ src/components/WYSIWYG/WYSIWYG.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-
 import dynamic from "next/dynamic";
 
 import "react-quill/dist/quill.snow.css";
 
-const WYSIWYG = dynamic(import("react-quill"), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-});
+const WYSIWYG = dynamic(
+  () => {
+    return import("react-quill");
+  },
+  { ssr: false }
+);
 
 export default WYSIWYG;
