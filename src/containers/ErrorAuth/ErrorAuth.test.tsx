7f2b5ea508f29465c5a77294b3ddd03154974092
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-04-03 23:18:38 +0100

    test: fixing test ErrorAuth container

--- src/containers/ErrorAuth/ErrorAuth.test.tsx
+++ src/containers/ErrorAuth/ErrorAuth.test.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { render, screen } from "@testing-library/react";
+import { SessionProvider } from "next-auth/react";
 
 import { ErrorAuth } from "@/containers";
 import content from "@/content";
 
 describe("Testing ErrorAuth container", () => {
   it("renders a ErrorAuth", () => {
-    render(<ErrorAuth />);
+    render(
+      <SessionProvider session={null}>
+        <ErrorAuth />
+      </SessionProvider>
+    );
 
     const element = screen.getByText(content.containers.errorAuth.text);
 
