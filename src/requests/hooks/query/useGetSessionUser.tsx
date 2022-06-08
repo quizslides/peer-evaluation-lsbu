01a8740d11920ea75793973f3a055e6c93a6257c
Gorka Guridi <gorka.guridi@gmail.com>
2022-06-08 14:53:15 +0100

    feat: Improve naming

--- src/requests/hooks/query/useGetSessionUser.tsx
+++ src/requests/hooks/query/useGetSessionUser.tsx
@@ -5,7 +5,7 @@ import { GET_SESSION_USER } from "@/requests/schema/user";
 import { errorNotification } from "@/utils";
 
 const useGetSessionUser = () => {
-  return useLazyQuery<{ users: User }, { where: UserWhereUniqueInput }>(GET_SESSION_USER, {
+  return useLazyQuery<{ user: User }, { where: UserWhereUniqueInput }>(GET_SESSION_USER, {
     fetchPolicy: "no-cache",
     onError: (error) => {
       errorNotification(error.message);
