f35358f816727ef4b599e81972ee5bfe22e2eccb
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2022-12-23 23:06:02 +0000

    fix: fixing security headers blocking js and style for page rendering
    PEL-345

--- next.config.js
+++ next.config.js
@@ -1,29 +1,49 @@
 /* eslint-disable @typescript-eslint/padding-line-between-statements */
+
 const { withSentryConfig } = require("@sentry/nextjs");
 const nextSafe = require("next-safe");
 
-const isProductionEnvironment = () => {
-  switch (process.env.ENVIRONMENT) {
-    case "ci":
-    case "local":
-      return false;
-    default:
-      return true;
-  }
-};
+const isProductionEnvironment = process.env.NODE_ENV === "production";
 
-let permissionsHeaders = [];
+let permissionsPolicyForTesting = {};
 
-const isProductionEnvironmentBuild = isProductionEnvironment();
-
-if (isProductionEnvironmentBuild) {
-  permissionsHeaders = [
-    {
-      source: "/:path*",
-      headers: nextSafe(),
-    },
-  ];
+if (!isProductionEnvironment) {
+  /**
+   * This header is needed for testing tool to communicate with the site automatically during their execution.
+   */
+  permissionsPolicyForTesting = {
+    "document-domain": "*",
+  };
 }
+
+const permissionsHeaders = [
+  {
+    source: "/:path*",
+    headers: nextSafe({
+      permissionsPolicy: {
+        /**
+         * Allow the site to copy content to clipboard.
+         */
+        "clipboard-write": "'*'",
+        ...permissionsPolicyForTesting,
+      },
+      contentSecurityPolicy: {
+        /**
+         * Due to libraries injecting in-line styles and using in-line styles and the bundle of Webpack these two directives need to be enabled.
+         */
+        "script-src": "'self' 'unsafe-eval'",
+        "style-src": "'self' 'unsafe-inline'",
+        /**
+         * Following Sentry Documentation for reporting added an external source to make requests.
+         *
+         * @source: https://docs.sentry.io/product/security-policy-reporting/
+         */
+        "connect-src": `'self' ${process.env.SENTRY_SECURITY_HEADER_ENDPOINT}`,
+      },
+      isDev: !isProductionEnvironment,
+    }),
+  },
+];
 
 const moduleExports = {
   poweredByHeader: false,
@@ -31,6 +51,7 @@ const moduleExports = {
   webpack: (config) => {
     config.experiments = config.experiments || {};
     config.experiments.topLevelAwait = true;
+
     return config;
   },
   async headers() {
@@ -42,6 +63,4 @@ const sentryWebpackPluginOptions = {
   silent: true,
 };
 
-module.exports = isProductionEnvironmentBuild
-  ? withSentryConfig(moduleExports, sentryWebpackPluginOptions)
-  : moduleExports;
+module.exports = isProductionEnvironment ? withSentryConfig(moduleExports, sentryWebpackPluginOptions) : moduleExports;
